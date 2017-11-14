da1e480e0c4fefa558291314ffe25e161ab32052
Unknown <shiyamrosyada14@gmail.com>
2017-11-14 14:13:44 +0700

    Upload

---
+++ src/if11_10116471_latihanuts3/If11_10116471_latihanuts3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if11_10116471_latihanuts3;
+
+/**
+ *
+ * @author thosiba c55-b5101
+ */
+public class If11_10116471_latihanuts3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Model.view().setVisible(true);
+    }
+    
+}
